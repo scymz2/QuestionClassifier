18d1fccf58a17e624d1ce583935f229ab7214054
oyxy <yxyomi>
2022-03-02 02:12:54 +0000

    3.1 add config & args parsers

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-"""
-@author:nicolasjam
-@file:__init__.py.py
-@time:2022/03/02
-"""
+
--- src/question_classifier.py
+++ src/question_classifier.py
@@ -3,45 +3,40 @@
 import torch
 import random
 import numpy as np
-import torch.nn as nn
-import utilty.preprocessing
+import configparser
+import argparse
+
+import utility.preprocessing
 from collections import Counter
+
 torch.manual_seed(1)
 random.seed(1)
 
-def train():
-    pass
+def train(config):
+    print("train_path:", config['PATH']['path_train'])
 
-def test():
-    pass
-
-def parse_args():
-    import argparse
-    parser = argparse.ArgumentParser(description='Parse command line arguments.')
-    parser.add_argument('--train', action='')
-    parser.add_argument('--test', action='')
-    parser.add_argument('--config', help='Configure parameters.')
-    args = parser.parse_args()
-    return args
-
-
-
+def test(config):
+    print("test_path", config['PATH']['path_train'])
 
 if __name__ == "__main__":
-    args = parse_args()
 
-    # load configurations
-    configs = load_configs()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True, default='../src/config.ini', help='Configuration file')
+    parser.add_argument('--train', action='store_true', help='Training mode - model is saved')
+    parser.add_argument('--test', action='store_true', help='Testing mode - needs a model to load')
+    args = parser.parse_args()
 
-    # preprocess
-
+    config_path = args.config
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read(config_path)
 
     # train
     if args.train:
-        train()
+        train(config)
 
     # test
-    if args.test:
-        test()
+    elif args.test:
+        test(config)
 
 
