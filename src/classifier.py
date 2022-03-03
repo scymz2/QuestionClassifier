d1d280d8e312469199acec60cd4f3b919dd9b35e
oyxy <yxyomi>
2022-03-03 20:28:38 +0000

    3.3 passing parameters/repair classifier

--- src/classifier.py
+++ src/classifier.py
@@ -3,7 +3,13 @@ import torch.nn as nn
 class Classifier(nn.Module):
     def __init_(self, n_input, n_hidden, n_output):
         super().__init_()
-        self.net = nn.Sequential(nn.Linear(n_input,n_hidden), nn.ReLU(), nn.Linear(n_hidden, n_output))
+        self.layer1 = nn.Linear(n_input, n_hidden)
+        self.layer2 = nn.Linear(n_hidden, n_hidden)
+        self.predict = nn.Linear(n_hidden, n_output)
 
     def forward(self, x):
-        return self.net(x)
+        out1 = self.layer1(x)
+        out2 = self.layer2(out1)
+        out = self.predict(out2)
+
+        return out
