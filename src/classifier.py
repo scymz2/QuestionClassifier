183254c742c35b30280dfbe37ae2793c984ec3d4
scymz2 <x001005xh@outlook.com>
2022-03-04 01:27:31 +0000

    utility part almost down.

---
+++ src/classifier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import torch.nn as nn
+
+
+class Classifier(nn.Module):
+    def __init_(self, n_input, n_hidden, n_output):
+        super().__init_()
+        self.net = nn.Sequential(nn.Linear(n_input, n_hidden), nn.ReLU(), nn.Linear(n_hidden, n_output))
+
+    def forward(self, x):
+        return self.net(x)
